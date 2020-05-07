102f3919f08e37d2b9fe555767f264ad953a78e2
yananren2137 <41330982+yananren2137@users.noreply.github.com>
2020-05-07 18:37:45 +0800

    Update args.py

--- utils/args.py
+++ utils/args.py
@@ -11,8 +11,8 @@ parser.add_argument('--snap_num', default=5, type=int, help='number of snapshot 
 parser.add_argument('--weight_decay', default=0, type=float, help='weight decay value of optimizer (default: 0)')
 parser.add_argument('--resize_scale', default=0.8, type=float, help='value of scale of random resized crop (default: 0.8)')
 parser.add_argument('--erasing_prob', default=0.2, type=float, help='prob of random erasing (default: 0.2)')
-parser.add_argument('--cutmix', default=True, type=bool, help='using cutmix (default: True)')
-parser.add_argument('--label_smooth', default=True, type=bool, help='using label smooth (default: True)')
+parser.add_argument("--cutmix", default='True', action='store_true', help='using cutmix (default: True)')
+parser.add_argument("--label_smooth", default='True', action='store_true', help='using label smooth (default: True)')
 parser.add_argument('--model_path', default=None, type=str, help='model path for resume training')
 
 train_args = parser.parse_args()
